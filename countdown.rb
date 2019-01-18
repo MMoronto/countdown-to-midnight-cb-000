624b8aa6641741837e85279e7833ff81de040c03
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-18 18:45:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,12 @@
 def countdown (number)
   counter = 0
   
-  while number > 0
+  while number > counter
   puts "#{number} SECOND(S)!"
   number = number - 1
   end
+  
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown(15)
